64496661c31e494044306d81d28892b4daecee0a
user <mourad.elbouaananii@gmail.com>
2018-01-18 09:44:47 +0100

    Connexion base de donnée

--- db.php
+++ db.php
@@ -1,2 +1,24 @@
 <?php
 /* Database connection settings */
+/*** mysql hostname ***/
+$hostname = 'localhost';
+
+/*** mysql username ***/
+$username = 'root';
+
+/*** mysql password ***/
+$password = '';
+
+$dbname = 'Page_Form';
+
+try {
+	$dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+	/*** echo a message saying we have connected ***/
+	//echo 'Connected to database';
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+    echo "Abort connection" . $e->getMessage();
+}
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 /* Main page with two forms: sign up and log in */
+require 'db.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,7 +10,20 @@
 </head>
 
 <?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
+    if (isset($_POST['login'])) { //user logging in
 
+        require 'login.php';
+        
+    }
+    
+    elseif (isset($_POST['register'])) { //user registering
+        
+        require 'register.php';
+        
+    }
+}
 ?>
 <body>
   <div class="form">
